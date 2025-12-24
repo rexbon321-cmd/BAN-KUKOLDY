3730608d5d3be18c1983a6b98d4384642e745aa4
rexbon321-cmd <rexbon321@gmail.com>
2025-12-24 15:08:48 +0100

    Update OpBanPro.java

--- src/main/java/pl/opban/OpBanPro.java
+++ src/main/java/pl/opban/OpBanPro.java
@@ -2,11 +2,34 @@ package pl.opban;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import pl.opban.core.BanManager;
-import pl.opban.commands.*;
+import pl.opban.commands.BanCommand;
+import pl.opban.commands.BanInfoCommand;
+import pl.opban.gui.BanMenuCommand;
+import pl.opban.gui.GuiListener;
+import pl.opban.gui.ChatInputListener;
 
 public class OpBanPro extends JavaPlugin {
- public void onEnable(){
-  BanManager.init(this);
-  getCommand("baninfo").setExecutor(new BanInfoCommand());
- }
+
+    @Override
+    public void onEnable() {
+
+        // init systemu banów
+        BanManager.init(this);
+
+        // === KOMENDY ===
+        getCommand("ban").setExecutor(new BanCommand());
+        getCommand("baninfo").setExecutor(new BanInfoCommand());
+        getCommand("banmenu").setExecutor(new BanMenuCommand());
+
+        // === LISTENERY ===
+        getServer().getPluginManager().registerEvents(new GuiListener(), this);
+        getServer().getPluginManager().registerEvents(new ChatInputListener(), this);
+
+        getLogger().info("OpBanPro ENABLED");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("OpBanPro DISABLED");
+    }
 }
